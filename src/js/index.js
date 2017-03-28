f936c62d378a27e083f8e0dc1d2b53f0d13c3898
luoxue <1904240744@qq.com>
2017-03-28 14:09:32 +0800

    edit

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
 import Scrollbar from './scrollbar';
+import ajax from './ajax';
+
+document.querySelector('.book').addEventListener('click', () => {
+    ajax({
+        url: '../about.html',
+        loadType: 'html',
+        success: (data) => {
+            document.querySelector('.book').innerHTML = data;
+        }
+    });
+});
 
 let scroller = new Scrollbar({
     element: '.book'
